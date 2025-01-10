af4f8587b2929b3dfe4b861adbd1a4e490512bbb
RahulDino <rc12082005@gmail.com>
2025-01-10 23:40:26 +0530

    add

--- mysite/static/scrollTransition.js
+++ mysite/static/scrollTransition.js
@@ -4,10 +4,12 @@ const observer = new IntersectionObserver((entries) => {
         if(entry.isIntersecting) {
             entry.target.classList.add('showLeft');
         }
+        else{
+            entry.target.classList.remove('showLeft');
+        }
 
     })
 })
-
 const hiddenElements = document.querySelectorAll('.hiddenLeft');
 hiddenElements.forEach((el) => observer.observe(el)); // This line is correct
 
@@ -22,6 +24,20 @@ const observer2 = new IntersectionObserver((entries) => {
         }
     })
 })
-
 const hiddenElements2 = document.querySelectorAll('.hiddenRight');
-hiddenElements2.forEach((el) => observer.observe(el));
+hiddenElements2.forEach((el) => observer2.observe(el));
+
+
+
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('showPop');
+        }
+        else{
+            entry.target.classList.remove('showPop');
+        }
+    })
+})
+const hiddenElements3 = document.querySelectorAll('.hiddenPop');
+hiddenElements3.forEach((el) => observer3.observe(el));
